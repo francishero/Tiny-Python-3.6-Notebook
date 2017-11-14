f07ed8f463fda8f25beee4e4210f4efeb6d78a59
Francis Miyoba <francismiyoba@gmail.com>
2017-11-13 23:17:44 -0800

    Add week one challenge

---
+++ week_one_challenge.py
@@ -0,0 +1,68 @@
+
+# this is week 1 challenge
+# our game currently has this output
+
+"""
+I am thinking of a number between 1 and 20.
+Take a guess.
+10
+Your guess is too low.
+Take a guess.
+15
+Your guess is too low.
+Take a guess.
+18
+Good job! You guessed my number in 3 guesses!
+"""
+#your challenge should you accept it is to rewrite using functions
+# define a function named guessing_game()
+#put all the code of the game in this function and it should still work
+# and produce this output
+
+"""
+I am thinking of a number between 1 and 20.
+Take a guess.
+10
+Your guess is too low.
+Take a guess.
+15
+Your guess is too low.
+Take a guess.
+18
+Good job! You guessed my number in 3 guesses!
+"""
+
+#now modify the game according to these rules
+# 1. keep track of each guessTime and print it to the user
+# use a function to do this
+
+# after running the code the output should look like this
+
+"""
+I am thinking of a number between 1 and 20.
+Take a guess.
+1
+Your guess is too low.
+your guess count is 1
+Take a guess.
+2
+Your guess is too low.
+your guess count is 2
+Take a guess.
+2
+Your guess is too low.
+your guess count is 3
+Take a guess.
+5
+Your guess is too high.
+your guess count is 4
+Take a guess.
+6
+Your guess is too high.
+your guess count is 5
+Take a guess.
+7
+Your guess is too high.
+your guess count is 6
+Nope. The number I was thinking of was 3
+"""
